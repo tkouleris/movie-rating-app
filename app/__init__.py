f90f384cadc2a7d858fcc6486ed534d33df14374
Thodoris Kouleris <kurokokoro@windowslive.com>
2022-01-27 06:34:16 +0200

    learn migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 db = SQLAlchemy(app)
-
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from app import routes
